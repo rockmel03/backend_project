2ec7717a4a398c2932476c2426b7c3dbf59ff719
rockmel03 <kamalswag2@gmail.com>
2024-04-15 01:51:05 +0530

    added controllers to get channel and subscribers lists

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -46,12 +46,106 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const { channelId } = req.params
 
-    
+    const check = isValidObjectId(channelId)
+    if (!check) throw new ApiError(400, "invalid channelId")
+
+    try {
+        const subscribers = await Subscription.aggregate([
+            {
+                $match: {
+                    channel: new mongoose.Types.ObjectId(channelId),
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "subscriber",
+                    foreignField: "_id",
+                    as: "subscriber",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                username: 1,
+                                fullName: 1,
+                                avatar: 1,
+                                coverImage: 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    subscriberData: { $first: "$subscriber" }
+                }
+            }
+        ])
+
+        const allSubscribersData = subscribers.map(sub => sub.subscriberData)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, allSubscribersData, "subscribers fetched successfully"))
+    } catch (error) {
+        throw new ApiError(500, "internal server error: " + error)
+    }
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    const check = isValidObjectId(subscriberId)
+    if (!check) throw new ApiError(400, "invalid subscriberId")
+
+    try {
+        const subscribedChannels = await Subscription.aggregate([
+            {
+                $match: {
+                    subscriber: new mongoose.Types.ObjectId(subscriberId),
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "channel",
+                    foreignField: "_id",
+                    as: "channel",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                username: 1,
+                                fullName: 1,
+                                avatar: 1,
+                                coverImage: 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    channelData: {
+                        $arrayElemAt: ["$channel", 0]
+                    }
+                }
+            }
+        ]
+        )
+
+        const allSubscribedChannels = subscribedChannels.map(chnl => chnl.channelData)
+
+        res.status(200)
+            .json(
+                new ApiResponse(200, allSubscribedChannels, "subscribed channels fetched successfully")
+            )
+    } catch (error) {
+        throw new ApiError(500, "internal server error: " + error)
+    }
 })
 
 export {
